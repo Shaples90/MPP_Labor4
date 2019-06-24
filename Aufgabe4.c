72d95c729c865e9272ef7ef8b5362ecb7f6f1f9a
Dae-Jin Seon <49846623+Shaples90@users.noreply.github.com>
2019-06-24 18:27:18 +0200

    Laborversuch 4 Aufgabe 4 funktioniert

--- Aufgabe4.c
+++ Aufgabe4.c
@@ -12,8 +12,8 @@ void wait(unsigned long timevalue)
 
 void configPorts(void)
 {
-   SYSCTL_RCGCGPIO_R |= (1 << 14);        // Port-Takt P freischalten
-   while(!(SYSCTL_PRGPIO_R & (1 << 14))); // Warte auf Port-Takt-Aktivierung 
+   SYSCTL_RCGCGPIO_R |= (1 << 13);        // Port-Takt P freischalten
+   while(!(SYSCTL_PRGPIO_R & (1 << 13))); // Warte auf Port-Takt-Aktivierung
    GPIO_PORTP_DEN_R |= (1 << 1);          // GPIO-Funktion für P(1) enablen
    GPIO_PORTP_AFSEL_R |= (1 << 1);        // Alternative-Funktion für P(1) enablen
    GPIO_PORTP_PCTL_R |= 0x1 << (4 * 1);   // UART-Funktion für P(1) aktivieren
@@ -25,9 +25,9 @@ void configUART(void)
    while(!(SYSCTL_PRUART_R & (1 << 6)));  // Warte auf Takt-Aktivierung bei UART6
    UART6_CTL_R &= ~0x01;                  // UART6 für Konfiguration disablen
 
-   UART6_IBRD_R = 781;                    // BRDI = int(BRD) = int(fcpu / 16 * bitrate) = int(120Mhz / 16 * 9600 bit/s)
-   UART6_FBRD_R = 16;                     // BRDF = round((BRD - BRDI) * 2^6)
-   UART6_LCRH_R = (0x3 << 5);             // 8/N/1 Format
+   UART6_IBRD_R = 104;                    // BRDI = int(BRD) = int(fcpu / 16 * bitrate) = int(16Mhz / 16 * 9600 bit/s)
+   UART6_FBRD_R = 11;                     // BRDF = round((BRD - BRDI) * 2^6)
+   UART6_LCRH_R = 0x60;             // 8/N/1 Format
    UART6_CTL_R |= ((1 << 8) | (1 << 0));  // UART6 und Transmit enable
 }
 
@@ -36,17 +36,17 @@ void lcDisplayFirstLine(void)
    unsigned char firstString[20] = "Versuchsteilnehmer:";
    unsigned int i = 0;
 
-   while((UART6_FR_R & 0x0020) != 0);     
-   UART6_DR_R = 0x7C;                     // LC-Display löschen                  
-   while((UART6_FR_R & 0x0020) != 0);     
+   while((UART6_FR_R & 0x0020) != 0);
+   UART6_DR_R = 0x7C;                     // LC-Display löschen
+   while((UART6_FR_R & 0x0020) != 0);
    UART6_DR_R = 0x2D;                     // Cursor 1.Zeile
 
    while(firstString[i] != '\0')
    {
       while((UART6_FR_R & 0x0020) != 0);
-      UART6_DR_R = firstString[i];
+      UART6_DR_R = (firstString[i]);
       i++;
-   } 
+   }
 }
 
 void lcDisplaySecondLine(void)
@@ -74,7 +74,7 @@ void lcDisplayThirdLine(void)
 
    while((UART6_FR_R & 0x0020) != 0);
    UART6_DR_R = 0xFE;                     // Cursor 3.Zeile Anfang
-   while((UART6_FR_R & 0x0020) != 0);  
+   while((UART6_FR_R & 0x0020) != 0);
    UART6_DR_R = 0x94;                     // Position 0
 
    while(thirdString[i] != '\0')
@@ -100,16 +100,18 @@ void lcDisplayForthLine(void)
       while((UART6_FR_R & 0x0020) != 0);
       UART6_DR_R = forthString[i];
       i++;
-   } 
+   }
 }
 
 void main(int argc, char const *argv[])
 {
    configPorts();                         // P(1) für UART konfiguieren
    configUART();                          // Bitrate: 9600 bit/s, Format: 8/N/1
-
-   lcDisplayFirstLine();                  // 'Versuchsteilnehmer:' anzeigen
-   lcDisplaySecondLine();                 // 'Mateo Narvaez' anzeigen
-   lcDisplayThirdLine();                  // 'Nikolas Rieder' anzeigen
-   lcDisplayForthLine();                  // 'Dae-Jin Seon' anzeigen
+   while(1)
+   {
+	   lcDisplayFirstLine();                  // 'Versuchsteilnehmer:' anzeigen
+	   lcDisplaySecondLine();                 // 'Mateo Narvaez' anzeigen
+	   lcDisplayThirdLine();                  // 'Nikolas Rieder' anzeigen
+	   lcDisplayForthLine();                  // 'Dae-Jin Seon' anzeigen
+   }
 }
